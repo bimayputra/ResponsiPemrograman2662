1e5ebc5a68ab04950f2a088f9b3b85ede9dff4b2
bimayputra <61857328+bimayputra@users.noreply.github.com>
2020-07-07 10:23:39 +0700

    ResponsiPemrograman2662

---
+++ Coustumer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectCostumer
+{
+    public class Costumer
+    {
+        // PERINTAH: lengkapi property class costumer, sesuai petunjuk soal
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Debt { get; set; }
+
+    }
+}
